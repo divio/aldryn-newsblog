b9d8227bd226dc3ce909bc93690da73df729a8ed
Tom Berger <tom.berger@gmail.com>
2014-12-10 15:46:55 +0100

    WIP

--- aldryn_newsblog/models.py
+++ aldryn_newsblog/models.py
@@ -4,14 +4,15 @@ from django.utils.translation import get_language, ugettext_lazy as _, override
 
 from cms.models.fields import PlaceholderField
 from parler.models import TranslatableModel, TranslatedFields
+# from filer.fields.image import FilerImageField
 
 
 class Article(TranslatableModel):
     translations = TranslatedFields(
-        title = models.CharField(_("Title"))
+        title = models.CharField(_('Title'), max_length=234),
         content = PlaceholderField(
             'aldryn_newsblog_article_content',
-            related_name='aldryn_newsblog_articles')
+            related_name='aldryn_newsblog_articles'),
         slug = models.SlugField(
             verbose_name=_('Slug'),
             max_length=255,
@@ -19,7 +20,7 @@ class Article(TranslatableModel):
             blank=True,
             help_text=_(
                 'Used in the URL. If changed, the URL will change. '
-                'Clean it to have it re-created.')
+                'Clean it to have it re-created.'),
         )
     )
     publication_start = models.DateTimeField(
@@ -28,4 +29,4 @@ class Article(TranslatableModel):
     )
     publication_end = models.DateTimeField(
         verbose_name=_('Published Until'), null=True, blank=True)
-    key_visual = FilerImageField(verbose_name=_('Key Visual'), blank=True, null=True)
+    # key_visual = FilerImageField(verbose_name=_('Key Visual'), blank=True, null=True)
