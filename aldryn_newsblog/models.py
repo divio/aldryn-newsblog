b216052a54e960b944aba52e3f1623acaaeac48f
Tom Berger <tom.berger@gmail.com>
2014-12-10 15:21:55 +0100

    WIP

---
+++ aldryn_newsblog/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import get_language, ugettext_lazy as _, override
+
+from cms.models.fields import PlaceholderField
+from parler.models import TranslatableModel, TranslatedFields
+
+
+class Article(TranslatableModel):
+    translations = TranslatedFields(
+        title = models.CharField(_("Title"))
+        content = PlaceholderField(
+            'aldryn_newsblog_article_content',
+            related_name='aldryn_newsblog_articles')
+        slug = models.SlugField(
+            verbose_name=_('Slug'),
+            max_length=255,
+            unique=True,
+            blank=True,
+            help_text=_(
+                'Used in the URL. If changed, the URL will change. '
+                'Clean it to have it re-created.')
+        )
+    )
+    publication_start = models.DateTimeField(
+        verbose_name=_('Published Since'), default=timezone.now,
+        help_text=_('Used in the URL. If changed, the URL will change.')
+    )
+    publication_end = models.DateTimeField(
+        verbose_name=_('Published Until'), null=True, blank=True)
+    key_visual = FilerImageField(verbose_name=_('Key Visual'), blank=True, null=True)
