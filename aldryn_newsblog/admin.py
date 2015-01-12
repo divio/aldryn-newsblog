49eecbf01063f40c20811d54bcab70bbed832d4a
Jonathan Stoppani <jonathan@stoppani.name>
2015-01-12 19:01:12 +0100

    WIP

--- aldryn_newsblog/admin.py
+++ aldryn_newsblog/admin.py
@@ -1,16 +1,30 @@
+from django.core.exceptions import PermissionDenied
 from django.contrib import admin
+from django.contrib.admin.util import unquote
+from django.db import transaction
+from django.shortcuts import get_object_or_404, render_to_response
+from django.utils.encoding import force_text
+from django.template import RequestContext
+
+from cms.admin.placeholderadmin import PlaceholderAdminMixin
+from cms.admin.placeholderadmin import FrontendEditableAdmin
+import reversion
+from reversion.models import Version
+from parler.admin import TranslatableAdmin
 
 from aldryn_apphooks_config.admin import BaseAppHookConfig
 from aldryn_people.models import Person
-from cms.admin.placeholderadmin import PlaceholderAdmin, FrontendEditableAdmin
-from parler.admin import TranslatableAdmin
 
-from .models import Article, NewsBlogConfig
+from .versioning import create_revision_with_placeholders
+from . import models
 
 
-class ArticleAdmin(TranslatableAdmin, PlaceholderAdmin, FrontendEditableAdmin):
+class ArticleAdmin(reversion.VersionAdmin,
+                   PlaceholderAdminMixin,
+                   TranslatableAdmin,
+                   FrontendEditableAdmin):
 
-    # TODO: make possible to edit placeholder
+    revision_confirmation_template = 'reversion/confirm_reversion.html'
 
     def add_view(self, request, *args, **kwargs):
         data = request.GET.copy()
@@ -22,11 +36,79 @@ class ArticleAdmin(TranslatableAdmin, PlaceholderAdmin, FrontendEditableAdmin):
             pass
         return super(ArticleAdmin, self).add_view(request, *args, **kwargs)
 
+    def edit_plugin(self, request, plugin_id):
+        with transaction.atomic():
+            with reversion.create_revision():
+                return super(ArticleAdmin, self).edit_plugin(
+                    request, plugin_id)
+
+    def _create_revision(self, plugin, user=None, comment=None):
+        objs = plugin.placeholder._get_attached_objects()
+        assert len(objs) == 1
+        obj = objs[0]
+        if user:
+            reversion.set_user(user)
+        if comment:
+            reversion.set_comment(comment)
+        create_revision_with_placeholders(obj)
+
+    def post_edit_plugin(self, request, plugin):
+        comment = u'Edited plugin #{0.id}: {0!s}'.format(plugin)
+        self._create_revision(plugin, request.user, comment)
+
+    def post_move_plugin(self, request, source_placeholder, target_placeholder,
+                         plugin):
+        comment = u'Moved plugin #{0.id}: {0!s}'.format(plugin)
+        self._create_revision(plugin, request.user, comment)
+
+    def post_delete_plugin(self, request, plugin):
+        comment = u'Deleted plugin #{0.id}: {0!s}'.format(plugin)
+        self._create_revision(plugin, request.user, comment)
+
+    @transaction.atomic
+    def revision_view(self, request, object_id, version_id,
+                      extra_context=None):
+        if not self.has_change_permission(request):
+            raise PermissionDenied()
+
+        obj = get_object_or_404(self.model, pk=unquote(object_id))
+        version = get_object_or_404(Version, pk=unquote(version_id),
+                                    object_id=force_text(obj.pk))
+        revision = version.revision
+
+        if request.method == "POST":
+            revision.revert()
+        else:
+            context = {
+                'object': obj,
+                'version': version,
+                'revision': revision,
+                'revision_date': revision.date_created,
+                'versions': revision.version_set.order_by(
+                    'content_type__name', 'object_id_int').all,
+                'object_name': force_text(self.model._meta.verbose_name),
+                'app_label': self.model._meta.app_label,
+                'opts': self.model._meta,
+                'add': False,
+                'change': True,
+                'save_as': False,
+                'has_add_permission': self.has_add_permission(request),
+                'has_change_permission': self.has_change_permission(
+                    request, obj),
+                'has_delete_permission': self.has_delete_permission(
+                    request, obj),
+                'has_file_field': True,
+                'has_absolute_url': False,
+                'original': obj,
+            }
+            return render_to_response(self.revision_confirmation_template,
+                                      context, RequestContext(request))
+
+admin.site.register(models.Article, ArticleAdmin)
+
 
 class NewsBlogConfigAdmin(BaseAppHookConfig):
     def get_config_fields(self):
         return []
 
-
-admin.site.register(Article, ArticleAdmin)
-admin.site.register(NewsBlogConfig, NewsBlogConfigAdmin)
+admin.site.register(models.NewsBlogConfig, NewsBlogConfigAdmin)
--- aldryn_newsblog/models.py
+++ aldryn_newsblog/models.py
@@ -1,5 +1,4 @@
 from __future__ import unicode_literals
-from functools import partial
 
 from django.core.urlresolvers import reverse
 from django.db import models
@@ -7,102 +6,20 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.utils.text import slugify
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
 
 from cms.models.fields import PlaceholderField
 from cms.models.pluginmodel import CMSPlugin
 from aldryn_people.models import Person
-import reversion
-from reversion.revisions import VersionAdapter
 from parler.models import TranslatableModel, TranslatedFields
-from parler import cache
 from aldryn_apphooks_config.models import AppHookConfig
 from aldryn_categories.fields import CategoryManyToManyField
 from taggit.managers import TaggableManager
 
+from .versioning import version_controlled_content
+
 
 class NewsBlogConfig(AppHookConfig):
     pass
-
-
-# TODO: The following classes and registration function shall be extracted in a
-# common add-on module (as soon as we have one).
-
-class TranslatableVersionAdapterMixin(object):
-    revision_manager = None
-
-    def __init__(self, model):
-        super(TranslatableVersionAdapterMixin, self).__init__(model)
-
-        # Register the translation model to be tracked as well, by following
-        # all placeholder fields, if any.
-        root_model = model._parler_meta.root_model
-        self.revision_manager.register(root_model)
-
-        # Also add the translations to the models to follow.
-        self.follow = list(self.follow) + [model._parler_meta.root_rel_name]
-
-        # And make sure that when we revert them, we update the translations
-        # cache (this is normally done in the translation `save_base` method,
-        # but it is not called when reverting changes).
-        post_save.connect(self._update_cache, sender=root_model)
-
-    def _update_cache(self, sender, instance, raw, **kwargs):
-        """Update the translations cache when restoring from a revision."""
-        if raw:
-            # Raw is set to true (only) when restoring from fixtures or
-            # with django-reversion (serialized data).
-            cache._cache_translation(instance)
-
-
-class PlaceholderVersionAdapterMixin(object):
-    follow_placeholders = True
-
-    def __init__(self, model):
-        super(PlaceholderVersionAdapterMixin, self).__init__(model)
-
-        # Add cms placeholders the to the models to follow.
-        placeholders = getattr(model._meta, 'placeholder_field_names', None)
-        if self.follow_placeholders and placeholders:
-            self.follow = list(self.follow) + placeholders
-            post_save.connect(self._add_plugins_to_revision, sender=model)
-
-    def _add_plugins_to_revision(self, sender, instance, **kwargs):
-        """Manually add plugins to the revision.
-
-        This method is an updated and adapter version of
-        https://github.com/divio/django-cms/blob/develop/cms/utils/helpers.py#L34
-        but instead of working on pages, works on models with placeholder
-        fields.
-        """
-        rev_ctx = self.revision_manager._revision_context_manager
-
-        def add_to_context(obj):
-            adapter = self.revision_manager.get_adapter(obj.__class__)
-            version_data = adapter.get_version_data(obj)
-            rev_ctx.add_to_context(self.revision_manager, obj, version_data)
-
-        if rev_ctx.is_active() and not rev_ctx.is_managing_manually():
-            ph_ids = [getattr(instance, '{0}_id'.format(name))
-                      for name in instance._meta.placeholder_field_names]
-
-            # Add all plugins to the revision
-            for plugin in CMSPlugin.objects.filter(placeholder_id__in=ph_ids):
-                plugin_instance, _ = plugin.get_plugin_instance()
-                if plugin_instance:
-                    add_to_context(plugin_instance)
-                add_to_context(plugin)
-
-
-class ContentEnabledVersionAdapter(TranslatableVersionAdapterMixin,
-                                   PlaceholderVersionAdapterMixin,
-                                   VersionAdapter):
-    pass
-
-
-version_controlled_content = partial(
-    reversion.register, adapter_cls=ContentEnabledVersionAdapter,
-    revision_manager=reversion.default_revision_manager)
 
 
 @python_2_unicode_compatible
@@ -113,7 +30,8 @@ class Article(TranslatableModel):
     )
 
     content = PlaceholderField('aldryn_newsblog_article_content',
-                               related_name='aldryn_newsblog_articles')
+                               related_name='aldryn_newsblog_articles',
+                               unique=True)
 
     slug = models.SlugField(
         verbose_name=_('Slug'),
@@ -128,7 +46,8 @@ class Article(TranslatableModel):
     author = models.ForeignKey(Person)
     owner = models.ForeignKey(User)
     namespace = models.ForeignKey(NewsBlogConfig)
-    categories = CategoryManyToManyField('aldryn_categories.Category', blank=True)
+    categories = CategoryManyToManyField('aldryn_categories.Category',
+                                         blank=True)
     tags = TaggableManager()
     publishing_date = models.DateTimeField()
 
@@ -136,7 +55,7 @@ class Article(TranslatableModel):
         ordering = ['-publishing_date']
 
     def __str__(self):
-        return self.title
+        return self.safe_translation_getter('title', any_language=True)
 
     def save(self, **kwargs):
         if not self.slug:
@@ -156,13 +75,14 @@ class LatestEntriesPlugin(CMSPlugin):
         help_text=_('The number of latest entries to be displayed.')
     )
 
-    def __str__(self):
-        return str(self.latest_entries).decode('utf8')
-
-    # TODO: make sure not to forget this if we add m2m/fk fields for _this_plugin_ later
+    # TODO: make sure not to forget this if we add m2m/fk fields for
+    # _this_plugin_ later:
     # def copy_relations(self, old_instance):
     #     self.categories = old_instance.categories.all()
     #     self.tags = old_instance.tags.all()
+
+    def __unicode__(self):
+        return u'Latest entries: {0}'.format(self.latest_entries)
 
     def get_articles(self):
         articles = Article.objects.active_translations()
