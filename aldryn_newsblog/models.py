44f8aeed63dd3f00195afbd5442309e0c8094412
Martin Koistinen <mkoistinen@gmail.com>
2015-01-27 14:48:26 +0200

    Left over from merge
    Conflicts:
    	aldryn_newsblog/models.py

--- aldryn_newsblog/models.py
+++ aldryn_newsblog/models.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 from django.conf import settings
@@ -37,9 +39,9 @@ else:
 @version_controlled_content
 class Article(TranslatableModel):
     translations = TranslatedFields(
-        title=models.CharField(_('Title'), max_length=234),
+        title=models.CharField(_('title'), max_length=234),
         slug=models.SlugField(
-            verbose_name=_('Slug'),
+            verbose_name=_('slug'),
             max_length=255,
             db_index=True,
             blank=True,
@@ -48,7 +50,7 @@ class Article(TranslatableModel):
                 'Clear it to have it re-created automatically.'),
         ),
         lead_in=HTMLField(
-            verbose_name=_('Lead-in'), default='',
+            verbose_name=_('lead-in'), default='',
             help_text=_('Will be displayed in lists, and at the start of the '
                         'detail page (in bold)')),
         meta_title=models.CharField(
@@ -58,19 +60,20 @@ class Article(TranslatableModel):
             verbose_name=_('meta description'), blank=True, default=''),
         meta_keywords=models.TextField(
             verbose_name=_('meta keywords'), blank=True, default=''),
-        meta={'unique_together': (('language_code', 'slug'),)},
+        meta={'unique_together': (('language_code', 'slug', ), )},
     )
 
     content = PlaceholderField('aldryn_newsblog_article_content',
                                related_name='aldryn_newsblog_articles',
                                unique=True)
-    author = models.ForeignKey(Person, null=True, blank=True)
-    owner = models.ForeignKey(User)
-    namespace = models.ForeignKey(NewsBlogConfig)
+    author = models.ForeignKey(Person, null=True, blank=True,
+        verbose_name=_('author'))
+    owner = models.ForeignKey(User, verbose_name=_('owner'))
+    namespace = models.ForeignKey(NewsBlogConfig, verbose_name=_('namespace'))
     categories = CategoryManyToManyField('aldryn_categories.Category',
-                                         blank=True)
+        blank=True, verbose_name=_('categories'))
     tags = TaggableManager(blank=True)
-    publishing_date = models.DateTimeField()
+    publishing_date = models.DateTimeField(_('publishing data'))
 
     featured_image = FilerImageField(null=True, blank=True)
 
@@ -147,11 +150,14 @@ class LatestEntriesPlugin(CMSPlugin):
         help_text=_('The number of latest entries to be displayed.')
     )
 
-    # TODO: make sure not to forget this if we add m2m/fk fields for
+    #
+    # NOTE: make sure not to forget this if we add m2m/fk fields for
     # _this_plugin_ later:
+    #
     # def copy_relations(self, old_instance):
     #     self.categories = old_instance.categories.all()
     #     self.tags = old_instance.tags.all()
+    #
 
     def __str__(self):
         return u'Latest entries: {0}'.format(self.latest_entries)
