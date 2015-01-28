9b9a7869edd0cbe8e51ca87a71565083ba8c47d2
Martin Koistinen <mkoistinen@gmail.com>
2015-01-28 03:14:12 +0200

    More active slugification

--- aldryn_newsblog/models.py
+++ aldryn_newsblog/models.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.core.urlresolvers import reverse
-from django.db import IntegrityError, models
+from django.db import connection, IntegrityError, models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.text import slugify as default_slugify
 from django.utils.translation import ugettext_lazy as _
@@ -93,8 +93,8 @@ class Article(TranslatableModel):
             'slug': self.safe_translation_getter('slug', any_language=True)
         }, current_app=self.namespace.namespace)
 
-    def slugify(self, category, i=None):
-        slug = default_slugify(category)
+    def slugify(self, source_text, i=None):
+        slug = default_slugify(source_text)
         if i is not None:
             slug += "_%d" % i
         return slug
@@ -109,38 +109,45 @@ class Article(TranslatableModel):
                                        self.owner.last_name))
                 })[0]
 
-        # Ensure there is a unique slug.
+        # Start with a naïve approach, if none provided.
         if not self.slug:
-            self.slug = self.slugify(self.name)
-            try:
-                return super(Article, self).save(*args, **kwargs)
-            except IntegrityError:
-                pass
+            self.slug = default_slugify(self.title)
 
-            for lang in LANGUAGE_CODES:
-                #
-                # We'd much rather just do something like:
-                #     Article.objects.translated(lang,
-                #         slug__startswith=self.slug)
-                # But sadly, this isn't supported by Parler/Django, see:
-                #     http://django-parler.readthedocs.org/en/latest/api/\
-                #         parler.managers.html#the-translatablequeryset-class
-                #
-                slugs = []
-                all_slugs = Article.objects.language(lang).exclude(
-                    id=self.id).values_list('translations__slug', flat=True)
-                for slug in all_slugs:
-                    if slug and slug.startswith(self.slug):
-                        slugs.append(slug)
-                i = 1
-                while True:
-                    slug = self.slugify(self.name, i)
-                    if slug not in slugs:
-                        self.slug = slug
-                        return super(Article, self).save(*args, **kwargs)
-                    i += 1
-        else:
+        # Ensure we aren't colliding with an existing slug *for this language*.
+        try:
+            if connection.vendor in ('sqlite', ):
+                # NOTE: This if statement should not be necessary, but testing
+                # is showing that SQLite is not respecting the unique_together
+                # constraint!
+                if Article.objects.translated(
+                        slug=self.slug).exclude(id=self.id).count():
+                    raise IntegrityError
             return super(Article, self).save(*args, **kwargs)
+        except IntegrityError:
+            pass
+
+        for lang in LANGUAGE_CODES:
+            #
+            # We'd much rather just do something like:
+            #     Article.objects.translated(lang,
+            #         slug__startswith=self.slug)
+            # But sadly, this isn't supported by Parler/Django, see:
+            #     http://django-parler.readthedocs.org/en/latest/api/\
+            #         parler.managers.html#the-translatablequeryset-class
+            #
+            slugs = []
+            all_slugs = Article.objects.language(lang).exclude(
+                id=self.id).values_list('translations__slug', flat=True)
+            for slug in all_slugs:
+                if slug and slug.startswith(self.slug):
+                    slugs.append(slug)
+            i = 1
+            while True:
+                slug = self.slugify(self.title, i)
+                if slug not in slugs:
+                    self.slug = slug
+                    return super(Article, self).save(*args, **kwargs)
+                i += 1
 
 
 @python_2_unicode_compatible
