c5a8a205ddd3bd192d7c80027eb46fefcc0e686b
Martin Koistinen <mkoistinen@gmail.com>
2015-01-28 14:39:58 +0200

    Fix migrations?

--- aldryn_newsblog/migrations/0001_initial.py
+++ aldryn_newsblog/migrations/0001_initial.py
@@ -14,7 +14,7 @@ import taggit.managers
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('aldryn_people', '0002_auto_20150127_2337'),
+        ('aldryn_people', '0001_initial'),
         ('taggit', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('cms', '0003_auto_20140926_2347'),
