8c7e95989d6324222b25f493381e86acb17cd1ff
Martin Koistinen <mkoistinen@gmail.com>
2015-01-30 04:23:22 +0200

    Remove dup. import

--- aldryn_newsblog/views.py
+++ aldryn_newsblog/views.py
@@ -5,8 +5,6 @@ from django.conf import settings
 from django.utils import translation
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-from parler.views import TranslatableSlugMixin
-
 from parler.views import TranslatableSlugMixin, ViewUrlMixin
 
 from aldryn_apphooks_config.mixins import AppConfigMixin
