2a0c428a2010fe34948ff36b11e03406d064693a
mikek <mike@openbunker.org>
2015-03-03 21:04:12 +0200

    Add test case to cover explicit manager/queryset 'published()' call

--- aldryn_newsblog/tests/test_aldryn_newsblog.py
+++ aldryn_newsblog/tests/test_aldryn_newsblog.py
@@ -172,7 +172,16 @@ class TestAldrynNewsBlog(NewsBlogTestsMixin, TransactionTestCase):
         response = self.client.get(article_url)
         self.assertEqual(response.status_code, 404)
 
-    def test_article_not_published(self):
+    def test_published_articles_filtering(self):
+        for i in range(5):
+            self.create_article()
+        unpublised_article = Article.objects.first()
+        unpublised_article.is_published = False
+        unpublised_article.save()
+        self.assertEqual(Article.objects.published().count(), 4)
+        self.assertNotIn(unpublised_article, Article.objects.published())
+
+    def test_view_article_not_published(self):
         article = self.create_article(is_published=False)
         response = self.client.get(article.get_absolute_url())
         self.assertEqual(response.status_code, 404)
