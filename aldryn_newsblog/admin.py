de6798510c97045c1bd6e4e08d383876bd4c1d79
Joseph Melettukunnel <jmelett@gmail.com>
2015-03-10 13:13:40 +0100

    More userfriendly creation process

--- aldryn_newsblog/admin.py
+++ aldryn_newsblog/admin.py
@@ -1,36 +1,42 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
-
 from cms.admin.placeholderadmin import FrontendEditableAdminMixin
-from parler.admin import TranslatableAdmin
 
+from parler.admin import TranslatableAdmin
 from aldryn_apphooks_config.admin import BaseAppHookConfig
 from aldryn_people.models import Person
 from aldryn_reversion.admin import VersionedPlaceholderAdminMixin
-
 from . import models
 
 
 def make_published(modeladmin, request, queryset):
     queryset.update(is_published=True)
+
+
 make_published.short_description = _(
     "Mark selected articles as published")
 
 
 def make_unpublished(modeladmin, request, queryset):
     queryset.update(is_published=False)
+
+
 make_unpublished.short_description = _(
     "Mark selected articles as not published")
 
 
 def make_featured(modeladmin, request, queryset):
     queryset.update(is_featured=True)
+
+
 make_featured.short_description = _(
     "Mark selected articles as featured")
 
 
 def make_not_featured(modeladmin, request, queryset):
     queryset.update(is_featured=False)
+
+
 make_not_featured.short_description = _(
     "Mark selected articles as not featured")
 
@@ -39,7 +45,6 @@ class ArticleAdmin(VersionedPlaceholderAdminMixin,
                    TranslatableAdmin,
                    FrontendEditableAdminMixin,
                    admin.ModelAdmin):
-
     list_display = ('title', 'app_config', 'slug', 'is_featured',
                     'is_published')
     actions = (
@@ -48,18 +53,18 @@ class ArticleAdmin(VersionedPlaceholderAdminMixin,
     )
     fieldsets = (
         (None, {
-            'fields': ('title', 'lead_in', )
+            'fields': ('title', 'featured_image',  )
         }),
         ('Details', {
             'classes': ('collapse',),
-            'fields': ('tags', 'categories', 'featured_image',
+            'fields': ('tags', 'categories', 'lead_in',
                        'publishing_date', 'is_published',)
         }),
         ('Meta options', {
             'classes': ('collapse',),
             'fields': ('meta_title', 'meta_description', 'meta_keywords')
         }),
-        ('Advanced options', {
+        ('Advanced', {
             'classes': ('collapse',),
             'fields': ('author', 'owner', 'app_config')
         }),
@@ -78,11 +83,13 @@ class ArticleAdmin(VersionedPlaceholderAdminMixin,
 
         return super(ArticleAdmin, self).add_view(request, *args, **kwargs)
 
+
 admin.site.register(models.Article, ArticleAdmin)
 
 
 class NewsBlogConfigAdmin(TranslatableAdmin, BaseAppHookConfig):
     def get_config_fields(self):
         return ('app_title', )
+
 
 admin.site.register(models.NewsBlogConfig, NewsBlogConfigAdmin)
