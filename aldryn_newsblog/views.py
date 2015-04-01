feab47205553227d63bb94d72e9c33abe5025e2b
Martin Koistinen <mkoistinen@gmail.com>
2015-04-01 20:37:58 +0300

    Content editors can always see articles, even unpublished ones.

--- aldryn_newsblog/views.py
+++ aldryn_newsblog/views.py
@@ -21,7 +21,34 @@ from aldryn_people.models import Person
 from .models import Article
 
 
-class ArticleDetail(TranslatableSlugMixin, AppConfigMixin, DetailView):
+class EditModeMixin(object):
+    """
+    A mixin which sets the property 'edit_mode' with the truth value for
+    whether a user is logged-into the CMS and is in edit-mode.
+    """
+    edit_mode = False
+
+    def dispatch(self, request, *args, **kwargs):
+        self.edit_mode = (
+            self.request.toolbar and self.request.toolbar.edit_mode)
+        return super(EditModeMixin, self).dispatch(request, *args, **kwargs)
+
+
+class PreviewModeMixin(EditModeMixin):
+    """
+    If content editor is logged-in, show all articles. Otherwise, only the
+    published articles should be returned.
+    """
+    def get_queryset(self):
+        qs = Article.objects
+        if not self.edit_mode:
+            qs = qs.published()
+        qs = qs.active_translations(
+            translation.get_language()).namespace(self.namespace)
+        return qs
+
+
+class ArticleDetail(PreviewModeMixin, TranslatableSlugMixin, AppConfigMixin, DetailView):
     model = Article
     slug_field = 'slug'
 
@@ -55,15 +82,8 @@ class ArticleDetail(TranslatableSlugMixin, AppConfigMixin, DetailView):
         else:
             return None
 
-    def get_queryset(self):
-        return Article.objects.published().active_translations(
-            translation.get_language()
-        ).namespace(
-            self.namespace
-        )
 
-
-class ArticleListBase(ViewUrlMixin, AppConfigMixin, ListView):
+class ArticleListBase(PreviewModeMixin, ViewUrlMixin, AppConfigMixin, ListView):
     model = Article
     show_header = False
 
@@ -75,14 +95,6 @@ class ArticleListBase(ViewUrlMixin, AppConfigMixin, ListView):
                 return self.config.paginate_by
             except AttributeError:
                 return 10  # sensible failsafe
-
-    @property
-    def queryset(self):
-        return Article.objects.published().active_translations(
-            translation.get_language()
-        ).namespace(
-            self.namespace
-        )
 
 
 class ArticleList(ArticleListBase):
@@ -107,17 +119,18 @@ class ArticleSearchResultsList(ArticleListBase):
         paginate by the app_config's settings.
         """
         return self.max_articles or super(
-            ArticleSearchResultsList, self).get_paginate_by(queryset)
+            ArticleSearchResultsList, self).get_paginate_by(self.get_queryset())
 
     def get_queryset(self):
+        qs = super(ArticleSearchResultsList, self).get_queryset()
         if self.query:
-            return self.queryset.filter(
+            return qs.filter(
                 Q(translations__title__icontains=self.query) |
                 Q(translations__lead_in__icontains=self.query) |
                 Q(translations__search_data__icontains=self.query)
             ).distinct()
         else:
-            return self.queryset.none()
+            return qs.none()
 
     def get_context_data(self, **kwargs):
         cxt = super(ArticleSearchResultsList, self).get_context_data(**kwargs)
@@ -133,12 +146,12 @@ class ArticleSearchResultsList(ArticleListBase):
 
 class AuthorArticleList(ArticleListBase):
     """A list of articles written by a specific author."""
-    @property
-    def queryset(self):
+    def get_queryset(self):
         # Note: each Article.author is Person instance with guaranteed
         # presence of unique slug field, which allows to use it in URLs
-        return super(AuthorArticleList, self).queryset.filter(
-            author=self.author)
+        return super(AuthorArticleList, self).get_queryset().filter(
+            author=self.author
+        )
 
     def get(self, request, author):
         self.author = get_object_or_404(Person, slug=author)
@@ -151,10 +164,10 @@ class AuthorArticleList(ArticleListBase):
 
 class CategoryArticleList(ArticleListBase):
     """A list of articles filtered by categories."""
-    @property
-    def queryset(self):
-        return super(CategoryArticleList, self).queryset.filter(
-            categories=self.category)
+    def get_queryset(self):
+        return super(CategoryArticleList, self).get_queryset().filter(
+            categories=self.category
+        )
 
     def get(self, request, category):
         self.category = get_object_or_404(
@@ -170,10 +183,10 @@ class CategoryArticleList(ArticleListBase):
 
 class TagArticleList(ArticleListBase):
     """A list of articles filtered by tags."""
-    @property
-    def queryset(self):
-        return super(TagArticleList, self).queryset.filter(
-            tags=self.tag)
+    def get_queryset(self):
+        return super(TagArticleList, self).get_queryset().filter(
+            tags=self.tag
+        )
 
     def get(self, request, tag):
         self.tag = get_object_or_404(Tag, slug=tag)
@@ -186,11 +199,11 @@ class TagArticleList(ArticleListBase):
 
 class DateRangeArticleList(ArticleListBase):
     """A list of articles for a specific date range"""
-    @property
-    def queryset(self):
-        return super(DateRangeArticleList, self).queryset.filter(
+    def get_queryset(self):
+        return super(DateRangeArticleList, self).get_queryset().filter(
             publishing_date__gte=self.date_from,
-            publishing_date__lt=self.date_to)
+            publishing_date__lt=self.date_to
+        )
 
     def _daterange_from_kwargs(self, kwargs):
         raise NotImplemented('Subclasses of DateRangeArticleList need to'
