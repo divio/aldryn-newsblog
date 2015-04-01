32d4f6dfd37c7cbc4500d25ec8857c7cfd54c118
Martin Koistinen <mkoistinen@gmail.com>
2015-04-02 01:51:17 +0300

    Manually merge changes from commit: 0f3e0d7
    0f3e0d78f48ea74813a5d595e1018986cda913e0

--- aldryn_newsblog/models.py
+++ aldryn_newsblog/models.py
@@ -182,8 +182,8 @@ class Article(TranslatableModel):
             self.slug = default_slugify(self.title)
 
         # Ensure we aren't colliding with an existing slug *for this language*.
-        if Article.objects.translated(
-                slug=self.slug).exclude(id=self.id).count() == 0:
+        if not Article.objects.translated(
+                slug=self.slug).exclude(pk=self.pk).exists():
             return super(Article, self).save(*args, **kwargs)
 
         for lang in LANGUAGE_CODES:
@@ -197,7 +197,7 @@ class Article(TranslatableModel):
             #
             slugs = []
             all_slugs = Article.objects.language(lang).exclude(
-                id=self.id).values_list('translations__slug', flat=True)
+                pk=self.pk).values_list('translations__slug', flat=True)
             for slug in all_slugs:
                 if slug and slug.startswith(self.slug):
                     slugs.append(slug)
@@ -274,7 +274,7 @@ class NewsBlogAuthorsPlugin(PluginEditModeMixin, NewsBlogCMSPlugin):
         ).values_list('author', flat=True).distinct()
 
         qs = Person.objects.filter(
-            id__in=list(queryset), article__app_config=self.app_config
+            pk__in=list(queryset), article__app_config=self.app_config
         ).annotate(
             count=models.Count('article')
         ).order_by('name')
@@ -297,7 +297,7 @@ class NewsBlogCategoriesPlugin(PluginEditModeMixin, NewsBlogCMSPlugin):
         ).values_list('categories', flat=True).distinct()
 
         qs = Category.objects.filter(
-            id__in=list(queryset),
+            pk__in=list(queryset),
             article__app_config=self.app_config,
         ).annotate(
             count=models.Count('article')
@@ -382,15 +382,17 @@ class NewsBlogTagsPlugin(PluginEditModeMixin, NewsBlogCMSPlugin):
         queryset = Article.objects
         if not self.edit_mode(request):
             queryset = queryset.published()
-        queryset = queryset.filter(app_config=self.app_config)
+        queryset = queryset.filter(
+            app_config=self.app_config
+        ).prefetch_related('tags')
 
         for article in queryset:
             for tag in article.tags.all():
-                if tag.id in tags:
-                    tags[tag.id].count += 1
+                if tag.pk in tags:
+                    tags[tag.pk].count += 1
                 else:
                     tag.count = 1
-                    tags[tag.id] = tag
+                    tags[tag.pk] = tag
         # Return most frequently used tags first
         return sorted(tags.values(), key=lambda x: x.count, reverse=True)
 
@@ -407,6 +409,6 @@ def update_seach_index(sender, instance, **kwargs):
         if hasattr(placeholder, '_attached_model_cache'):
             if placeholder._attached_model_cache == Article:
                 article = placeholder._attached_model_cache.objects.get(
-                    content=placeholder.id)
+                    content=placeholder.pk)
                 article.search_data = article.get_search_data(instance.language)
                 article.save()
