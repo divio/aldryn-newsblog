c027b1173a9baa2fc3b63ee38ee3fd54b50c958c
Felipe Prenholato <philipe.rp@gmail.com>
2015-04-13 02:44:41 -0300

    Make the search indexer failsafe.

--- aldryn_newsblog/search_indexes.py
+++ aldryn_newsblog/search_indexes.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
-
+from django.core.urlresolvers import NoReverseMatch
 from aldryn_search.utils import get_index_base
 
 from .models import Article
@@ -15,10 +15,18 @@ class ArticleIndex(get_index_base()):
     index_title = True
 
     def get_title(self, obj):
-        return obj.safe_translation_getter('title')
+        language = self.prepared_data['language']
+        return obj.safe_translation_getter('title', language_code=language)
+
+    def get_url(self, obj):
+        try:
+            return obj.get_absolute_url()
+        except NoReverseMatch:
+            return ''
 
     def get_description(self, obj):
-        return obj.safe_translation_getter('lead_in')
+        language = self.prepared_data['language']
+        return obj.safe_translation_getter('lead_in', language_code=language)
 
     def index_queryset(self, using=None):
         self._get_backend(using)
@@ -37,6 +45,5 @@ class ArticleIndex(get_index_base()):
         return Article
 
     def get_search_data(self, article, language, request):
-        return article.safe_translation_getter(
-            'search_data', language_code=language
-        )
+        return article.safe_translation_getter('search_data', language_code=language)
+
