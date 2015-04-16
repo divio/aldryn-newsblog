e81788c797c095326ab335873c3c8e32ec44ab95
Martin Koistinen <mkoistinen@gmail.com>
2015-04-17 01:23:06 +0300

    Add CMSAttachMenu to support breadcrumbs

--- aldryn_newsblog/cms_app.py
+++ aldryn_newsblog/cms_app.py
@@ -3,13 +3,15 @@ from cms.apphook_pool import apphook_pool
 from django.utils.translation import ugettext_lazy as _
 
 from .models import NewsBlogConfig
+from .menu import NewsBlogMenu
 
 
 class NewsBlogApp(CMSConfigApp):
+    app_config = NewsBlogConfig
+    app_name = 'aldryn_newsblog'
+    menus = [NewsBlogMenu, ]
     name = _('NewsBlog')
     urls = ['aldryn_newsblog.urls']
-    app_name = 'aldryn_newsblog'
-    app_config = NewsBlogConfig
 
 
 apphook_pool.register(NewsBlogApp)
