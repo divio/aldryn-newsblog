280dc6b4c7e9f00d9c267b73736e274ee74c1e27
Piotr Jakimiak <piotrj@dcs.pl>
2015-04-17 14:05:28 +0200

    Add languages support

--- aldryn_newsblog/sitemaps/sitemap.py
+++ aldryn_newsblog/sitemaps/sitemap.py
@@ -11,10 +11,13 @@ class NewsBlogSitemap(Sitemap):
 
     def __init__(self, *args, **kwargs):
         self.namespace = kwargs.pop('namespace', None)
+        self.language = kwargs.pop('language', None)
         super(NewsBlogSitemap, self).__init__(*args, **kwargs)
 
     def items(self):
         qs = Article.objects.published()
+        if self.language is not None:
+            qs = qs.translated(self.language)
         if self.namespace is not None:
             qs = qs.filter(app_config__namespace=self.namespace)
         return qs
--- aldryn_newsblog/tests/test_sitemaps.py
+++ aldryn_newsblog/tests/test_sitemaps.py
@@ -4,40 +4,52 @@ from __future__ import unicode_literals
 
 from . import NewsBlogTestCase
 from aldryn_newsblog.sitemaps import NewsBlogSitemap
+
 from django.contrib.sites.models import get_current_site
+from django.utils.translation import override
 
 class TestSitemaps(NewsBlogTestCase):
+
+    def _sitemap_urls(self, sitemap):
+        urls_info = sitemap.get_urls()
+        urls = [url_info['location'] for url_info in urls_info]
+        return urls
+
+    def _article_urls(self, articles):
+        host = 'http://' + get_current_site(self.request).domain
+        return [host + article.get_absolute_url() for article in articles]
+
+    def assertArticlesIn(self, articles, sitemap):
+        urls = self._sitemap_urls(sitemap)
+        article_urls = self._article_urls(articles)
+
+        for url in article_urls:
+            self.assertIn(url, urls)
+
+    def assertArticlesNotIn(self, articles, sitemap):
+        urls = self._sitemap_urls(sitemap)
+        article_urls = self._article_urls(articles)
+
+        for url in article_urls:
+            self.assertNotIn(url, urls)
 
     def test_listening_all_instances(self):
         articles = [self.create_article() for _ in range(11)]
         unpublished_article = articles[0]
         unpublished_article.is_published = False
         unpublished_article.save()
-        urls_info = NewsBlogSitemap().get_urls()
-        urls = [url_info['location'] for url_info in urls_info]
-
-        host = 'http://' + get_current_site(self.request).domain
-
-        self.assertEqual(len(articles[1:]), len(urls))
-        for article in articles[1:]:
-            self.assertIn(host + article.get_absolute_url(), urls)
-        self.assertNotIn(host + unpublished_article.get_absolute_url(), urls)
+        sitemap = NewsBlogSitemap()
+        self.assertArticlesIn(articles[1:], sitemap)
+        self.assertArticlesNotIn([unpublished_article], sitemap)
 
     def test_listening_namespace(self):
         articles = [self.create_article() for _ in range(11)]
         unpublished_article = articles[0]
         unpublished_article.is_published = False
         unpublished_article.save()
-        urls_info = NewsBlogSitemap(
-            namespace=self.app_config.namespace).get_urls()
-        urls = [url_info['location'] for url_info in urls_info]
-
-        host = 'http://' + get_current_site(self.request).domain
-
-        self.assertEqual(len(articles[1:]), len(urls))
-        for article in articles[1:]:
-            self.assertIn(host + article.get_absolute_url(), urls)
-        self.assertNotIn(host + unpublished_article.get_absolute_url(), urls)
+        sitemap = NewsBlogSitemap(namespace=self.app_config.namespace)
+        self.assertArticlesIn(articles[1:], sitemap)
+        self.assertArticlesNotIn([unpublished_article], sitemap)
 
     def test_listening_unexisting_namespace(self):
         articles = [self.create_article() for _ in range(11)]
@@ -47,3 +59,24 @@ class TestSitemaps(NewsBlogTestCase):
         sitemap = NewsBlogSitemap(
             namespace='not exists')
         self.assertFalse(sitemap.items())
+        self.assertArticlesNotIn(articles, sitemap)
+
+    def test_languages_support(self):
+        with override('en'):
+            multilanguage_article = self.create_article()
+            en_article = self.create_article()
+
+        multilanguage_article.create_translation('de', title='DE title')
+        with override('de'):
+            de_article = self.create_article()
+
+        multilingual_sitemap = NewsBlogSitemap()
+        self.assertArticlesIn([multilanguage_article, en_article, de_article], multilingual_sitemap)
+
+        en_sitemap = NewsBlogSitemap(language='en')
+        self.assertArticlesIn([multilanguage_article, en_article], en_sitemap)
+        self.assertArticlesNotIn([de_article], en_sitemap)
+
+        de_sitemap = NewsBlogSitemap(language='de')
+        self.assertArticlesIn([multilanguage_article, de_article], de_sitemap)
+        self.assertArticlesNotIn([en_article], de_sitemap)
