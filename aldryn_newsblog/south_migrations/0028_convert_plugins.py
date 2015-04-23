7f53b390a60a1cdc220243b43037a80efdeeadc4
Martin Koistinen <mkoistinen@gmail.com>
2015-04-23 20:09:57 +0300

    Do it directly, rather than through the orm

--- aldryn_newsblog/south_migrations/0028_convert_plugins.py
+++ aldryn_newsblog/south_migrations/0028_convert_plugins.py
@@ -4,6 +4,8 @@ from south.db import db
 from south.v2 import DataMigration
 from django.db import models
 
+from cms.models import CMSPlugin
+
 
 class Migration(DataMigration):
     no_dry_run = True
@@ -18,26 +20,23 @@ class Migration(DataMigration):
     )
 
     def forwards(self, orm):
-        try:
-            for plugin in orm['cms.CMSPlugin'].objects.all():
-                for m in self.mapping:
-                    if plugin.plugin_type == m[0]:
-                        plugin.plugin_type = m[1]
-                        plugin.save()
-                        break
-        except:
-            pass
+        """
+        We've opted to prefix all plugins with the addon name: "NewsBlog"
+        """
+        for plugin in CMSPlugin.objects.all():
+            for m in self.mapping:
+                if plugin.plugin_type == m[0]:
+                    plugin.plugin_type = m[1]
+                    plugin.save()
+                    break
 
     def backwards(self, orm):
-        try:
-            for plugin in orm['cms.CMSPlugin'].objects.all():
-                for m in self.mapping:
-                    if plugin.plugin_type == m[1]:
-                        plugin.plugin_type = m[0]
-                        plugin.save()
-                        break
-        except:
-            pass
+        for plugin in CMSPlugin.objects.all():
+            for m in self.mapping:
+                if plugin.plugin_type == m[1]:
+                    plugin.plugin_type = m[0]
+                    plugin.save()
+                    break
 
     models = {
         u'aldryn_categories.category': {
