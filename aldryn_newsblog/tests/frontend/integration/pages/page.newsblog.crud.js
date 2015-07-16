1b3e6634b6c7e0c1852ff211d883c0f2fc85be59
Marketionist <marketionist@gmail.com>
2015-07-16 15:29:19 +0300

    Added login test

--- aldryn_newsblog/tests/frontend/integration/pages/page.newsblog.crud.js
+++ aldryn_newsblog/tests/frontend/integration/pages/page.newsblog.crud.js
@@ -9,11 +9,49 @@
 // #############################################################################
 // INTEGRATION TEST PAGE OBJECT
 
-var newsblogPage = {
+var newsBlogPage = {
     site: 'http://127.0.0.1:8000/en/',
     mainElementsWaitTime: 12000,
     iframeWaitTime: 15000,
 
+    // log in, log out
+    editModeLink: element(by.css('.inner a[href="/?edit"]')),
+    usernameInput: element(by.id('id_cms-username')),
+    passwordInput: element(by.id('id_cms-password')),
+    loginButton: element(by.css('.cms_form-login input[type="submit"]')),
+    userMenus: element.all(by.css('.cms_toolbar-item-navigation > li > a')),
+    testLink: element(by.css('.selected a')),
+
+    cmsLogin: function (credentials) {
+        // object can contain username and password, if not set it will
+        // fallback to 'admin'
+        credentials = credentials ||
+            { username: 'admin', password: 'admin' };
+
+        newsBlogPage.usernameInput.clear();
+
+        // fill in email field
+        newsBlogPage.usernameInput.sendKeys(
+            credentials.username).then(function () {
+            newsBlogPage.passwordInput.clear();
+
+            // fill in password field
+            newsBlogPage.passwordInput.sendKeys(
+                credentials.password);
+        }).then(function () {
+            newsBlogPage.loginButton.click();
+
+            // wait for user menu to appear
+            browser.wait(browser.isElementPresent(
+                newsBlogPage.userMenus.first()),
+                newsBlogPage.mainElementsWaitTime);
+
+            // validate user menu
+            expect(newsBlogPage.userMenus.first().isDisplayed())
+                .toBeTruthy();
+        });
+    }
+
 };
 
-module.exports = newsblogPage;
+module.exports = newsBlogPage;
