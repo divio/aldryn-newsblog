fe3be20c7f7e57597d0ff1f0550b8fd0f0401e24
Paulo <commonzenpython@gmail.com>
2016-03-16 11:49:15 -0400

    flake8 fixes

--- aldryn_newsblog/views.py
+++ aldryn_newsblog/views.py
@@ -211,9 +211,11 @@ class ArticleListBase(AppConfigMixin, AppHookCheckMixin, TemplatePrefixMixin,
                 'pages_start': 10,
                 'pages_visible': 4,
             }
-        options['pages_visible_negative'] = -options['pages_visible']
+
+        pages_visible_negative = -options['pages_visible']
+        options['pages_visible_negative'] = pages_visible_negative
         options['pages_visible_total'] = options['pages_visible'] + 1
-        options['pages_visible_total_negative'] = options['pages_visible_negative'] - 1
+        options['pages_visible_total_negative'] = pages_visible_negative - 1
         return options
 
     def get_context_data(self, **kwargs):
