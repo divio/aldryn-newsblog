c7a0aa7f36fc61dd5edd3c2c6a1a12812905ccb1
Bartosz Płóciennik <vThaian@users.noreply.github.com>
2019-02-05 14:10:56 +0100

    Bumped version to 2.2.0 (#509)

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-2.2.0 (unreleased)
+2.2.0 (2019-02-05)
 ==================
 
 * Added support for Django 2.0 and 2.1
--- aldryn_newsblog/__init__.py
+++ aldryn_newsblog/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 
-__version__ = '2.1.4'
+__version__ = '2.2.0'
 
 
 default_app_config = 'aldryn_newsblog.apps.AldrynNewsBlog'
